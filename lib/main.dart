4f84ddde484d58e52c570b9cd9fd4ebe5f08dbdd
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-04 21:38:21 +0300

    Learn Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Learn Flutter",
-      home: Text("Hello, World!",),
-    )
+  runApp(
+    MaterialApp(
+        title: "Learn Flutter",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Learn flutter"),
+            centerTitle: true,
+          ),
+          body: Text("Hello, World"),
+          floatingActionButton: FloatingActionButton(
+            child: Text("Add"),
+          ),
+          bottomNavigationBar: BottomNavigationBar(),
+        )),
   );
 }
