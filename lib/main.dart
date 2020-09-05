826dc4f10ededcfb75bcf12fc1b86eb597525a15
Maxim <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-05 15:54:08 +0300

    Learn Card widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,28 @@ void main() => runApp(FlutterTutorialApp());
 
 class FlutterTutorialApp extends StatelessWidget {
   final List<Event> events = [
-    Event(name: "Name 1", location: "Location 1", startDateTime: DateTime.now() ),
-    Event(name: "Name 2", location: "Location 2", startDateTime: DateTime.now() ),
-    Event(name: "Name 3", location: "Location 3", startDateTime: DateTime.now() ),
-    Event(name: "Name 4", location: "Location 4", startDateTime: DateTime.now() ),
-    Event(name: "Name 5", location: "Location 5", startDateTime: DateTime.now() ),
-    Event(name: "Name 6", location: "Location 6", startDateTime: DateTime.now() ),
-    Event(name: "Name 7", location: "Location 7", startDateTime: DateTime.now() ),
-    Event(name: "Name 8", location: "Location 8", startDateTime: DateTime.now() ),
-    Event(name: "Name 9", location: "Location 9", startDateTime: DateTime.now() ),
-    Event(name: "Name 10", location: "Location 10", startDateTime: DateTime.now() ),
+    Event(
+        name: "Name 1", location: "Location 1", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 2", location: "Location 2", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 3", location: "Location 3", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 4", location: "Location 4", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 5", location: "Location 5", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 6", location: "Location 6", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 7", location: "Location 7", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 8", location: "Location 8", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 9", location: "Location 9", startDateTime: DateTime.now()),
+    Event(
+        name: "Name 10",
+        location: "Location 10",
+        startDateTime: DateTime.now()),
   ];
 
   @override
@@ -32,9 +44,28 @@ class FlutterTutorialApp extends StatelessWidget {
             physics: BouncingScrollPhysics(),
             padding: EdgeInsets.all(40),
             itemCount: events.length,
-            itemBuilder: (_, index) => Text(
-              "$index - ${events[index].name} - ${events[index].location} - ${events[index].startDateTime}",
-              style: TextStyle(fontSize: 20),
+            itemBuilder: (_, index) => Card(
+              color: Colors.red[300],
+              elevation: 10,
+              shadowColor: Colors.lightBlueAccent,
+              margin: EdgeInsets.symmetric(vertical: 7),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                children: [
+                  Text(
+                    events[index].name,
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(events[index].location),
+                      SizedBox(width: 10,),
+                      Text(events[index].startDateTime.toString()),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
           floatingActionButton: FloatingActionButton(
